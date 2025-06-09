c369d31dd4ca5ac25a48e88af7ed6b6c910ac85a
sdntmn <sdntmn@mail.ru>
2025-06-09 22:44:38 +0500

    Добавлены e2e тесты с Cypress

---
+++ cypress/e2e/common/routyng.cy.ts
@@ -0,0 +1,32 @@
+import { selectByTestId } from "cypress/helpers/selectByTestId"
+
+describe("Роутинг", () => {
+  describe("Пользователь НЕ авторизован", () => {
+    it("Переход на главную страницу", () => {
+      cy.visit("/")
+      cy.get(selectByTestId("MainPage")).should("exist")
+    })
+    it("Редирект при попытке перехода на страницу Профиля", () => {
+      cy.visit("/profile/1")
+      cy.get(selectByTestId("MainPage")).should("exist")
+    })
+    it("Редирект при попытке открыть не существующую страницу", () => {
+      cy.visit("/qwerty")
+      cy.get(selectByTestId("NotFoundPage")).should("exist")
+    })
+  })
+
+  describe("Пользователь авторизован", () => {
+    beforeEach(() => {
+      cy.login("admin", "123")
+    })
+    it("Переход на страницу Профиля", () => {
+      cy.visit("/profile/1")
+      cy.get(selectByTestId("ProfilePage")).should("exist")
+    })
+    it("Переход на страницу со списком статей", () => {
+      cy.visit("/articles")
+      cy.get(selectByTestId("ArticlesPage")).should("exist")
+    })
+  })
+})
